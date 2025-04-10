85e49623dc09d810888790a909660bf97a0b9c10
andyb <andyb@solvento.mx>
2025-04-10 11:13:40 -0500

    uodate var env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,5 @@ declare const process: any;
 
 export const environment = {
   production: true,
-  apiBaseUrl:
-    process.env['API_URL'] ||
-    'https://camps-app-back-production.up.railway.app/api/',
+  apiBaseUrl: 'https://camps-app-back-production.up.railway.app/api/',
 };
