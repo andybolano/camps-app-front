d9be2228caa9f39d4d91a26d840254be5bddfc14
andyb <andyb@solvento.mx>
2025-04-10 11:17:49 -0500

    remove /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ declare const process: any;
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://camps-app-back-production.up.railway.app/api/',
+  apiBaseUrl: 'https://camps-app-back-production.up.railway.app/api',
 };
