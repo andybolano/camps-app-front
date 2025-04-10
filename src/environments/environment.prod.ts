a91f753116ab8d3a1d3d58f19abc9877027e2de7
andyb <andyb@solvento.mx>
2025-04-10 10:48:53 -0500

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   apiBaseUrl:
     process.env['API_URL'] ||
-    'https://captivating-unity-production.up.railway.app/api',
+    'https://camps-app-back-production.up.railway.app/api/',
 };
